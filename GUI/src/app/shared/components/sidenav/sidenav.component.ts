9ae9ca26ced73531eb38a715b7918783d5089475
liz-veronica <87659504+liz-veronica@users.noreply.github.com>
2022-08-20 19:26:07 -0400

    implementación de la interfaz gráfica

---
+++ GUI/src/app/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {AuthenticationService} from "../../../authentication/authentication.service";
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  constructor(private authenticationService: AuthenticationService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggle() {
+  }
+
+  isAuthenticated(): Observable<boolean> {
+      return this.authenticationService.isAuthenticated();
+  }
+
+  seeAdministrationMenu() {
+      return true
+  }
+
+  checkRoles() {
+      return true
+  }
+
+}
