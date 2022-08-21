771aa6c2ccacfdc7761e4efd026853bb845ff3a3
Robert Meyer <rmeyer93@fpuna.edu.py>
2022-08-21 10:03:38 -0400

    Update on authcontroller

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -2,14 +2,15 @@ const { request, response } = require('express');
 const  { Usuario }  = require('../models/usuarios');
 
 const login = async (req = request, res = response) => {
-    const usuario = await Usuario.findOne({ where: { email: req.body.email } });
-    if(usuario)
+    const {email, contrasenha} = req.body;
+    const usuario = await Usuario.findOne({email, contrasenha})
+    if(usuario.contrasenha == contrasenha)
     res.json({
         msg: 'Login correcto'
     });
     else{
         res.status(401).json({
-            msg: 'Usuario no existe'
+            msg: 'Usuario no existe o password no valido.'
         });
     }
 };
