4d249760f65965e45bf41ce4bcbc437278e8ffff
Robert Meyer <rmeyer93@fpuna.edu.py>
2022-08-21 22:27:17 -0400

    Ajustes en el login de usuario

--- CORE/controllers/authcontroller.js
+++ CORE/controllers/authcontroller.js
@@ -1,16 +1,22 @@
 const { request, response } = require('express');
 const   Usuario   = require('../models/usuarios');
+const bcrypt = require ('bcryptjs');
 
 const login = async (req = request, res = response) => {
-    const usuario = await Usuario.findOne({ where :
-                     {email: req.body.email, 
-                      password: req.body.password
-                     }});
+    const usuario = await Usuario.findOne({ where : {email: req.body.email}});
     if(usuario){
-        res.status(200).json({
-            msg: 'login correcto'
-        });
-    }
+        const validarPassword = bcrypt.compareSync(req.body.password, usuario.password)
+        if(validarPassword){
+            res.status(200).json({
+                msg: 'login correcto'
+            });
+        }
+        else{
+            res.json({
+                msg: 'Contrasenha no valida.'
+            })
+        }
+    }                                        
     else
         res.status(401).json({
             msg: 'Usuario o contrasenha no valido.'
